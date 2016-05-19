8789869415a75cee9bafa9394fbe77176211219d
Stan Behrens <git@sbeh.de>
2016-05-19 16:28:28 +0200

    Introducte the admin close and add a bit more security to users-service.

--- src/services/user/hooks/index.js
+++ src/services/user/hooks/index.js
@@ -5,38 +5,67 @@ const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication').hooks;
 
 exports.before = {
-  all: [],
+  all: [
+    // can't put verifyToken/populateUser/restrictTo... here, because create has to be possible without beeing authenticated
+  ],
   find: [
     auth.verifyToken(),
     auth.populateUser(),
-    auth.restrictToAuthenticated()
+    auth.restrictToAuthenticated(),
+    auth.restrictToRoles({ roles: ['admin'] })
   ],
   get: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
+    auth.restrictToRoles({
+      roles: ['admin'],
+      ownerField: '_id',
+      owner: true
+    })
   ],
   create: [
-    auth.hashPassword()
+    auth.hashPassword(),
+    // don't allow to assign yourself roles on user creation
+    hooks.remove('roles')
   ],
   update: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
+    auth.restrictToRoles({
+      roles: ['admin'],
+      ownerField: '_id',
+      owner: true
+    }),
+    // don't allow to assign yourself roles, unless you're an admin
+    hooks.remove('roles', hook => {
+      return !hook.params.user.roles.contains('admin')
+    })
   ],
   patch: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
+    auth.restrictToRoles({
+      roles: ['admin'],
+      ownerField: '_id',
+      owner: true
+    }),
+    // don't allow to assign yourself roles, unless you're an admin
+    hooks.remove('roles', hook => {
+      return !hook.params.user.roles.contains('admin')
+    })
   ],
   remove: [
     auth.verifyToken(),
     auth.populateUser(),
     auth.restrictToAuthenticated(),
-    auth.restrictToOwner({ ownerField: '_id' })
+    auth.restrictToRoles({
+      roles: ['admin'],
+      ownerField: '_id',
+      owner: true
+    })
   ]
 };
 
