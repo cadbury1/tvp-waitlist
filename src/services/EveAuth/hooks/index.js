694065ffd3586a24045a995a0db9a593d8c6cf2e
Stan Behrens <git@sbeh.de>
2016-05-31 15:09:31 +0200

    Remove hook AuthGenerateState and embed the code into hooks/index.js

--- src/services/EveAuth/hooks/index.js
+++ src/services/EveAuth/hooks/index.js
@@ -2,32 +2,33 @@
 
 const eveAuthCreateSsoLink = require('./eveAuthCreateSsoLink');
 
-const eveAuthGenerateState = require('./eveAuthGenerateState');
-
 const globalHooks = require('../../../hooks');
 const hooks = require('feathers-hooks');
 const auth = require('feathers-authentication').hooks;
 
 exports.before = {
-  all: [
-    auth.verifyToken(),
-    auth.populateUser(),
-    auth.restrictToAuthenticated()
-  ],
-  find: hooks.disable('external'),
-  get: hooks.disable('external'),
-  create: [eveAuthGenerateState()],
-  update: hooks.disable('external'),
-  patch: hooks.disable('external'),
-  remove: hooks.disable('external')
+     all: [globalHooks.log({service: 'EveAuth'})],
+    find:  hooks.disable('external'),
+     get:  hooks.disable('external'), // TODO refresh token on get
+  create: [auth.verifyToken(),
+           auth.populateUser(),
+           auth.restrictToAuthenticated(),
+           
+           hooks.pluck('scope'), // removes everything but data.scope
+           auth.associateCurrentUser(), // adds data.userId
+           hook => { hook.data.state = '' + Math.random() }], // adds data.state
+  update:  hooks.disable('external'),
+   patch:  hooks.disable('external'),
+  remove:  hooks.disable('external')
 };
 
 exports.after = {
-  all: [],
-  find: [],
-  get: [],
-  create: [eveAuthCreateSsoLink()],
+     all: [], // TODO pluck access_token
+    find: [],
+     get: [],
+  create: [eveAuthCreateSsoLink(), // adds result.ssoLink
+           hooks.pluck('ssoLink')], // removes everything but result.ssoLink
   update: [],
-  patch: [],
+   patch: [],
   remove: []
 };
